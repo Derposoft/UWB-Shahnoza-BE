8f1266d59050fdf75d63fc7c7afd2bcfc985e679
Derposoft <viswanath.chadalapaka@gmail.com>
2020-04-17 12:14:39 -0700

    +listen

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const express = require('express')
 const app = express()
+const port = 3000 //devel port
 
 app.route(express.static('views'))
 
+app.listen(port, () => {
+    console.log('listening on port ' + port)
+})
