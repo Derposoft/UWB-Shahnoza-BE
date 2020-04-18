1b7ecad7568e1da2936c3d9385c6965b121667a8
Derposoft <viswanath.chadalapaka@gmail.com>
2020-04-17 23:51:46 -0700

    -ebs,azure junk. +working code

--- server.js
+++ server.js
@@ -9,9 +9,6 @@ const vision = require('@google-cloud/vision')
 const app = express()
 const port = 8081 //devel port
 
-const handleError = (err, res) => {
-    res.status(500).contentType("text/plain").end("Oops! Something went wrong!")
-}
 const upload = multer({ dest: "./temp/" })
 
 // parse application/json, application/x-www-form-urlencoded
@@ -28,45 +25,46 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.static('./views'))
 //app.get("/", express.static(path.join(__dirname, "./views")));
 
-async function infer(fileName, callback) {
+async function infer(res, fileName, callback) {
     const client = new vision.ImageAnnotatorClient()
     // Detect similar images on the web to a local file
     const [result] = await client.webDetection(fileName)
-    callback(result)
+    callback(res, result)
 }
-var pricefetch = (result) => {
+var pricefetch = (res, result) => {
     // build url from which to scrape
     var query = ""
-    var entities =result.webEntities
+    var entities = result.webDetection.webEntities
     if (entities.length == 0)
-        sendback("image match not found", 500)
+        sendback("image match not found", 69)
         
     for (var i = 0; i < entities.length; i++)
-        query += entities[key] + " "
+        query += entities[i].description + " "
     // remove final space
     query = query.substr(0, query.length - 1)
     var url = "https://www.google.com/search?psb=1&tbm=shop&q=" + query
     // get back html for the site
     
     // call final callback
-    sendres(JSON.stringify(result), 200)
+    sendres(res, result, 200)
 }
 
 // callback to send result
-var sendres = (result, status) => {
+var sendres = (res, json, status) => {
     res.status = status
-    res.send(JSON.stringify(result))
+    res.send(JSON.stringify(json))
 }
 
 app.post('/upload',
-    upload.single("object"),
+    upload.single("image"),
     (req, res, next) => {
         console.log("FILE:" + req.file)
-        fs.renameSync(req.file.path, path.join(__dirname, "test.png"))//req.body.name))
-        
+        console.log("FNAME:" + req.body.name)
+        fs.renameSync(req.file.path, path.join(__dirname + '/temp', req.body.name))
+
         const filepath = path.join(__dirname, "./temp/" + req.body.name)
         // get image inference
-        infer(filepath, pricefetch, sendres)
+        infer(res, filepath, pricefetch, sendres)
     }
 )
 
