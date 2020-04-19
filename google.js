90782bf546274f9c3442a1f6dee34c761ad14a31
jaygrinols <jaygrinols@gmail.com>
2020-04-18 17:29:44 -0700

    added images functionality

--- google.js
+++ google.js
@@ -1,35 +1,22 @@
-const request = require('request')
-const url = 'https://www.google.com/search?q=Bass+guitar+Electric+guitar+Acoustic+Guitar+Slide+guitar+Cuatro+Acoustic-electric+guitar+Tiple+Guitar+/m/083vt+Double+bass+Wood+steel-string+acoustic+guitar&sxsrf=ALeKk015ZqfOrkZQveUZPM3t1du9qsSQrg:1587172418214&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiX-byB5vDoAhXJup4KHT8sBfIQ_AUoAXoECAsQAw&biw=1920&bih=977'
+async function urlData(url) {
+    parseHtml = html => {
+        const cheerio = require('cheerio')
+        const $ = cheerio.load(html)
+        var priceCheerio = $(".HRLxBb").contents()
+        var urlCheerio = $(".rgHvZc").contents()
+        var nameCheerio = $(".rgHvZc")
+        var imgCheerio = $(".rgHvZc").parent().parent().find("img")
+        var out = []
+        for (var i = 0; i < priceCheerio.length; i++) {
+            out.push({name: nameCheerio.eq(i).text(), price: priceCheerio[i].data, url: "https://www.google.com" + urlCheerio[i].attribs.href, imgsrc: imgCheerio[i].attribs.src})
+        }
+        return out;
+    }
 
-const url1 = 'https://www.google.com/search?q=Bass+guitar+Electric+guitar+Acoustic+Guitar+Slide+guitar+Cuatro+Acoustic-electric+guitar+Tiple+Guitar+/m/083vt+Double+bass+Wood+steel-string+acoustic+guitar&sxsrf=ALeKk039MsV8SyfUOeWotU-Rl_PKAdOlVw:1587191253494&source=lnms&sa=X&ved=0ahUKEwiPiuuWrPHoAhWW9Z4KHbu6AmsQ_AUIlgUoAA&biw=674&bih=1283'
-request(url, function(error, response, body) {
-    parseHtml(body);
-})
-
-parseHtml = (html) => {
-    const cheerio = require('cheerio')
-    const $ = cheerio.load(html)
-//    console.log($.html())
-//url: $(".HRLxBb").parent().parent().find("a").attr('href')
-//
-//    console.log($(".HRLxBb").parent().parent().contents().get(0))
-var priceCheerio = $(".HRLxBb").contents()
-var urlCheerio = $(".rgHvZc").contents()
-var prices = []
-var urls = []
-var names = []
-for (var i = 0; i < priceCheerio.length; i++) {
-    prices.push(priceCheerio[i].data)
-    urls.push(urlCheerio[i].attribs.href)
-}
-console.log(prices)
-console.log(urls)
+    const axios = require('axios')
+    var reqData = axios.get(url).then(resp => parseHtml(resp.data))
+    return reqData
 }
 
-
-//const html = '<ul id="fruits"><li class="apple">Apple</li><li class="orange">Orange</li><li class="pear">Pear</li></ul>'
-//const $ = cheerio.load(html)
-//console.log(html)
-//
-//console.log($('li[class=orange]').html())
-
+const myurl = "https://www.google.com/search?q=Bass+guitar+Electric+guitar+Acoustic+Guitar+Slide+guitar+Cuatro+Acoustic-electric+guitar+Tiple+Guitar+/m/083vt+Double+bass+Wood+steel-string+acoustic+guitar&sxsrf=ALeKk015ZqfOrkZQveUZPM3t1du9qsSQrg:1587172418214&source=lnms&tbm=shop&sa=X&ved=2ahUKEwiX-byB5vDoAhXJup4KHT8sBfIQ_AUoAXoECAsQAw&biw=1920&bih=977"
+var out = urlData(myurl).then(res => console.log(res))
